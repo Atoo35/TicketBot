32c2025e64bee2a457ec4faac753e39632a04ef3
Atoo35 <adrooney322@gmail.com>
2020-05-05 16:09:08 +0530

    fixes

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ client.on("guildCreate", async (guild) => {
 client.on("guildDelete", async(guild) => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  var channel = await guild.channels.find(c=>c.name==='ticket-bot')
-  channel.delete();
+  // var channel = await guild.channels.find(c=>c.name==='ticket-bot')
+  // channel.delete();
   client.user.setPresence({ game: { name: `issues in ${client.guilds.size} servers.`,type:"Listening" }, status: 'online' })
 
 
